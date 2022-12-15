15c3339cc2cf608049ea6645183399f1f933e0fb
Min276 <minkhantkyawttu@gmail.com>
2022-12-15 17:53:21 +0800

    asxox shop and app section done

---
+++ components/AsxoxShopAppSection/index.tsx
@@ -0,0 +1,43 @@
+import { Box, Typography } from "@mui/material";
+import { FunctionComponent } from "react";
+import Image from "next/image";
+
+const AsxoxShopAppSection: FunctionComponent = () => {
+  return (
+    <Box className="px-24 pt-14 py-20 bg-[#0C2E5C]">
+      <Typography className="!text-2xl !font-semibold text-white text-center py-8 pb-12">
+        About our Asxox Shop & App
+      </Typography>
+      <Box className="flex items-center">
+        <Box className="w-1/2">
+          <Image
+            width={380}
+            height={380}
+            src="/assets/asxoxImage.svg"
+            alt="logo"
+          />
+        </Box>
+        <Box className="w-1/2 flex flex-col justify-end">
+          <Typography className="!text-xl !font-semibold text-white">
+            Asxox Shop and App Feature
+          </Typography>
+          <Typography className="text-white !font-medium py-4 !leading-7">
+            Lorem ipsum dolor sit amet consectetur. Posuere ipsum donec
+            porttitor euismod ultrices tristique. Mauris tortor consequat lectus
+            consequat mi. Facilisi tortor in fringilla risus pellentesque ac
+            lectus.
+          </Typography>
+          <ul className="list-disc pl-4 pb-8 text-white !leading-10">
+            <li>Lorem ipsum dolor sit amet consectetur sit amet </li>
+            <li>Lorem ipsum dolor sit amet consectetur sit amet </li>
+          </ul>
+          <Box className="px-5 py-3 w-fit rounded-md text-[#0C2E5C] bg-white cursor-pointer !font-semibold">
+            Call an action
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default AsxoxShopAppSection;
