ad0557a02bc4abf369955fe71735a460121cba2a
Min276 <minkhantkyawttu@gmail.com>
2022-12-20 11:35:09 +0800

    more languages feature updated

--- components/DCTAppSection/index.tsx
+++ components/DCTAppSection/index.tsx
@@ -1,22 +1,29 @@
 import { Box, Typography } from "@mui/material";
+import { useTranslation } from "next-i18next";
 import { FunctionComponent } from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
 
 const DCTAppSection: FunctionComponent = () => {
+  const { t } = useTranslation();
   return (
     <Box className="px-6 pt-10 pb-14 md:px-24 md:pt-14 md:py-20 bg-white">
-<motion.div
-            initial={{ scale: 0.8, opacity: 0 }}
-            whileInView={{ scale: 1, opacity: 1, transition: { delay: 0.4, duration: 0.5} }}
-          >        <Typography className="!text-xl md:!text-2xl !font-semibold text-[#0C2E5C] text-center py-8 md:pb-14">
-          Easy scan & buy with DCT App
+      <motion.div
+        initial={{ scale: 0.8, opacity: 0 }}
+        whileInView={{
+          scale: 1,
+          opacity: 1,
+          transition: { delay: 0.4, duration: 0.5 },
+        }}
+      >
+        <Typography className="!text-xl md:!text-2xl !font-semibold text-[#0C2E5C] text-center py-8 md:pb-14">
+          {t("landing:dctAppSection.title")}
         </Typography>
       </motion.div>
       <Box className="flex flex-col md:flex-row items-center">
         <Box className="w-full md:w-1/2">
           <Typography className="!text-lg md:!text-xl !font-semibold text-[#0C2E5C] pb-4">
-            Our app features include these:
+            {t("landing:dctAppSection.subTitle")}
           </Typography>
           <ul className="list-disc pl-4 pb-8 text-[#1E1D1D] !leading-10">
             <li>
@@ -43,9 +50,12 @@ const DCTAppSection: FunctionComponent = () => {
             <Box className="flex items-center md:pl-14 pb-8 md:pb-0">
               <Box className="flex flex-col text-[#1E1D1D] pr-6">
                 <Typography className="text-[#0C2E5C] !font-semibold pb-2">
-                  To download our App
+                  {t("landing:dctAppSection.downloadTitle")}
                 </Typography>
-                <Typography>Scan QR code here:</Typography>
+                <Typography className="text-[#1E1D1D] !font-semibold">
+                  {" "}
+                  {t("landing:dctAppSection.scanQRTitle")}
+                </Typography>
               </Box>
               <Image
                 width={100}
@@ -57,16 +67,20 @@ const DCTAppSection: FunctionComponent = () => {
           </Box>
         </Box>
         <Box className="w-full md:w-1/2 flex justify-center md:justify-end">
-        <motion.div
+          <motion.div
             initial={{ scale: 0.8, opacity: 0 }}
-            whileInView={{ scale: 1, opacity: 1, transition: { delay: 0.4, duration: 0.5 } }}
+            whileInView={{
+              scale: 1,
+              opacity: 1,
+              transition: { delay: 0.4, duration: 0.5 },
+            }}
           >
-          <Image
-            width={500}
-            height={500}
-            src="/assets/dctAppSection/dctAppMockup.svg"
-            alt="dct app mockup"
-          />
+            <Image
+              width={500}
+              height={500}
+              src="/assets/dctAppSection/dctAppMockup.svg"
+              alt="dct app mockup"
+            />
           </motion.div>
         </Box>
       </Box>
--- components/DCTSection/index.tsx
+++ components/DCTSection/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography } from "@mui/material";
+import { useTranslation } from "next-i18next";
 import { FunctionComponent } from "react";
 import Image from "next/image";
 import { FaMapMarkerAlt } from "react-icons/fa";
@@ -6,21 +7,25 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 
 const DCTSection: FunctionComponent = () => {
+  const { t } = useTranslation();
   return (
     <Box className="px-6 pt-10 pb-18 md:px-24 md:pt-14 md:py-20 bg-white">
       <motion.div
         initial={{ scale: 0.8, opacity: 0 }}
-        whileInView={{ scale: 1, opacity: 1, transition: { delay: 0.4, duration: 0.5 } }}
+        whileInView={{
+          scale: 1,
+          opacity: 1,
+          transition: { delay: 0.4, duration: 0.5 },
+        }}
       >
-        {" "}
         <Typography className="!text-[1.35rem] md:!text-2xl !font-semibold text-[#0C2E5C] text-center py-8 md:pb-12">
-          Let&apos;s take a glance at DCT
+          {t("landing:dctSection.title")}
         </Typography>
       </motion.div>
       <Box className="flex flex-col md:flex-row items-center">
         <Box className="w-full md:w-1/2">
           <Typography className="!text-lg md:!text-xl !font-semibold text-[#0C2E5C]">
-            Digital Cloud Technology
+            {t("landing:dctSection.subTitle")}
           </Typography>
           <Typography className="text-[#0C2E5C] !font-medium py-4 !leading-7">
             Lorem ipsum dolor sit amet consectetur. Posuere ipsum donec
@@ -32,7 +37,7 @@ const DCTSection: FunctionComponent = () => {
             <Box className="flex items-center">
               <FaMapMarkerAlt className="text-[#EF5353] !text-xl" />
               <Typography className="pl-2 text-[#0C2E5C] !font-semibold">
-                Our Location
+                {t("landing:dctSection.location")}
               </Typography>
             </Box>
             <Typography className="pt-2 pb-8 text-[#1E1D1D] pl-7">
@@ -43,21 +48,29 @@ const DCTSection: FunctionComponent = () => {
           </Box>
           <motion.div
             initial={{ scale: 0.8, opacity: 0 }}
-            whileInView={{ scale: 1, opacity: 1, transition: { delay: 0.4, duration: 0.5 } }}
+            whileInView={{
+              scale: 1,
+              opacity: 1,
+              transition: { delay: 0.4, duration: 0.5 },
+            }}
           >
             <Link
               target="_blank"
               className="px-5 py-3 w-fit rounded-md text-white bg-[#0C2E5C] cursor-pointer !font-semibold"
               href="https://goo.gl/maps/WxThXFRDcAw1rKqBA"
             >
-              View in Google Map
+              {t("landing:dctSection.button")}
             </Link>
           </motion.div>
         </Box>
         <Box className="w-full md:w-1/2 flex justify-center md:justify-end py-12 md:py-0">
           <motion.div
             initial={{ scale: 0.8, opacity: 0 }}
-            whileInView={{ scale: 1, opacity: 1, transition: { delay: 0.4, duration: 0.5 } }}
+            whileInView={{
+              scale: 1,
+              opacity: 1,
+              transition: { delay: 0.4, duration: 0.5 },
+            }}
           >
             <Image
               width={500}
