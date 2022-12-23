e5fa2f958aaf6497375cfc3bd96fdaea61c8e757
Min276 <minkhantkyawttu@gmail.com>
2022-12-23 13:05:53 +0800

    card ui components done

--- components/FacebookPackages/CardComponent/index.tsx
+++ components/FacebookPackages/CardComponent/index.tsx
@@ -1,41 +1,37 @@
 import { Box, Typography } from "@mui/material";
 import { FunctionComponent } from "react";
-import Image from "next/image";
 import { motion } from "framer-motion";
-import {BsFillCheckCircleFill} from "react-icons/bs";
+import { BsFillCheckCircleFill } from "react-icons/bs";
 
 interface CardProps {
+  key: number;
   title: string;
   tab: string;
   textColor?: string;
   backgroundColor?: string;
-  itemOne: string;
-  itemTwo: string;
-  itemThree: string;
-  itemFour: string;
-  itemFive?: string;
-  itemSix: string;
+  restData?: any;
 }
 
 const CardComponent: FunctionComponent<CardProps> = ({
+  key,
   title,
   tab,
   textColor,
   backgroundColor,
-  itemOne,
-  itemTwo,
-  itemThree,
-  itemFour,
-  itemFive,
-  itemSix,
+  restData,
 }) => {
   return (
     <motion.div
       initial={{ scale: 0.8, opacity: 0 }}
-      whileInView={{ scale: 1, opacity: 1, transition: { delay: 0.4, duration: 0.5 } }}
+      whileInView={{
+        scale: 1,
+        opacity: 1,
+        transition: { delay: 0.4, duration: 0.5 },
+      }}
     >
       <Box
-        className="relative w-4/5 px-10 py-14 rounded-md cursor-pointer"
+        key={key}
+        className="relative w-4/5 h-[468px] px-16 py-10 rounded-md cursor-pointer"
         sx={{
           backgroundColor: backgroundColor,
           boxShadow: "0px 1px 10px rgba(0, 0, 0, 0.25);",
@@ -46,32 +42,23 @@ const CardComponent: FunctionComponent<CardProps> = ({
             <Typography className=" !font-medium !text-lg">{tab}</Typography>
           </Box>
           <Typography
-            className="flex !text-xl !font-semibold items-end gap-x-2 pt-12"
+            className="flex !text-xl !font-semibold items-end gap-x-2 mb-6 pt-14"
             sx={{ color: textColor }}
           >
-            <Typography className="!text-3xl !font-semibold">{title} </Typography> per month
+            <Typography className="!text-3xl !font-semibold">
+              {title}{" "}
+            </Typography>{" "}
+            per month
           </Typography>
-          <Box className="flex flex-col gap-y-6">
-             <Typography className="flex text-[#0C2E5C]">
-                <BsFillCheckCircleFill className="text-xl"/> &nbsp; {itemOne}
-             </Typography>
-             <Typography className="flex text-[#0C2E5C]">
-                <BsFillCheckCircleFill />  &nbsp; {itemTwo}
-             </Typography>
-             <Typography className="flex text-[#0C2E5C]">
-                <BsFillCheckCircleFill />  &nbsp; {itemThree}
-             </Typography>
-             <Typography className="flex text-[#0C2E5C]">
-                <BsFillCheckCircleFill />  &nbsp; {itemFour}
-             </Typography>
-             { itemFive &&
-              (<Typography className="flex text-[#0C2E5C]">
-                <BsFillCheckCircleFill />  &nbsp; {itemFive}
-             </Typography>
-              )}
-              <Typography className="flex text-[#0C2E5C]">
-                <BsFillCheckCircleFill />  &nbsp; {itemSix}
-             </Typography>
+          <Box className="flex flex-col gap-y-4">
+            {restData?.map((item: any, index: number) => (
+              <Box className="flex" key={index}>
+                <BsFillCheckCircleFill className="!text-xl w-8 text-[#0C2E5C]" />
+                <Typography className="pl-2 text-base w-64 text-[#1E1D1D]">
+                  {item}
+                </Typography>
+              </Box>
+            ))}
           </Box>
         </Box>
       </Box>
--- components/FacebookPackages/index.tsx
+++ components/FacebookPackages/index.tsx
@@ -8,31 +8,30 @@ import { useTranslation } from "next-i18next";
 const CardComponent = dynamic(import("./CardComponent"), { ssr: false });
 
 const FacebookPackages: FunctionComponent = () => {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   return (
     <Box className="px-6 py-10 md:px-24 md:pt-14 md:pb-20 bg-white">
       <motion.div
         initial={{ scale: 0.8, opacity: 0 }}
-        whileInView={{ scale: 1, opacity: 1, transition: { delay: 0.4, duration: 0.5 } }}
+        whileInView={{
+          scale: 1,
+          opacity: 1,
+          transition: { delay: 0.4, duration: 0.5 },
+        }}
       >
         <Typography className="!text-[1.3rem] md:!text-2xl !font-semibold text-[#0C2E5C] text-center py-8">
           {t("landing:digitalMarketing.facebookMarketing.title")}
         </Typography>
       </motion.div>
       <Box className="flex flex-wrap justify-center md:justify-between gap-y-8 mt-2">
-        {FacebookPackagesData.map((data, index: number) => (
+        {FacebookPackagesData.map((data: any, index: number) => (
           <CardComponent
             key={index}
             title={data.pricing}
             tab={data.plan}
             textColor={data.textColor}
             backgroundColor={data.backgroundColor}
-            itemOne={data.content}
-            itemTwo={data.photo}
-            itemThree={data.promotion}
-            itemFour={data.message}
-            itemFive={data.video}
-            itemSix={data.boost}
+            restData={data.restData}
           />
         ))}
       </Box>
