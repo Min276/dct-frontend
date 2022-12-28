f4b0a9ae75c40727c528b3d23851b3e6bfe0c2e1
Min276 <minkhantkyawttu@gmail.com>
2022-12-28 17:01:18 +0800

    banner refactoring done

--- components/BannerSection/index.tsx
+++ components/BannerSection/index.tsx
@@ -2,22 +2,19 @@ import { Box, Typography } from "@mui/material";
 import { useTranslation } from "next-i18next";
 import { FunctionComponent } from "react";
 import dynamic from "next/dynamic";
-import { motion } from "framer-motion";
 
+const AnimationFrame = dynamic(import("../AnimationFrame"), { ssr: false });
 const SearchFeature = dynamic(import("../SearchFeature"), { ssr: false });
 
-const BannerSection: FunctionComponent= () => {
-  const {t} = useTranslation();
+const BannerSection: FunctionComponent = () => {
+  const { t } = useTranslation();
   return (
     <Box className="relative flex justify-center items-center w-full h-[43vh] sm:h-[54vh] md:h-[60vh] bg-no-repeat bg-center bg-cover bg-blend-multiply bg-[url('/assets/banner.svg')]">
-      <motion.div
-        initial={{ scale: 0.8, opacity: 0 }}
-        whileInView={{ scale: 1, opacity: 1, transition: { delay: 0.4 , duration: 0.5 } }}
-      >
+      <AnimationFrame>
         <Typography className="px-8 text-center text-white !text-2xl md:!text-4xl !font-semibold !leading-10">
-        {t("landing:title")}
+          {t("landing:title")}
         </Typography>
-      </motion.div>
+      </AnimationFrame>
       <SearchFeature />
     </Box>
   );
--- components/SoftwareDevBanner/index.tsx
+++ components/SoftwareDevBanner/index.tsx
@@ -1,22 +1,21 @@
 import { Box, Typography } from "@mui/material";
 import { useTranslation } from "next-i18next";
 import { FunctionComponent } from "react";
-import { motion } from "framer-motion";
-import BreadcrumbsComponent from "../Breadcrumbs";
+import dynamic from "next/dynamic";
 
-const SoftwareDevBanner: FunctionComponent= () => {
-  const {t} = useTranslation();
+const AnimationFrame = dynamic(import("../AnimationFrame"), { ssr: false });
+const BreadcrumbsComponent = dynamic(import("../Breadcrumbs"), { ssr: false });
+
+const SoftwareDevBanner: FunctionComponent = () => {
+  const { t } = useTranslation();
   return (
     <Box className="flex justify-center items-center w-full h-[43vh] sm:h-[54vh] md:h-[60vh] bg-no-repeat bg-center bg-cover bg-blend-multiply bg-[url('/assets/softwareBanner.svg')]">
-      <motion.div
-        initial={{ scale: 0.8, opacity: 0 }}
-        whileInView={{ scale: 1, opacity: 1, transition: { delay: 0.4 , duration: 0.5 } }}
-      >
+      <AnimationFrame>
         <Typography className="px-8 text-center text-white !text-2xl md:!text-4xl !font-semibold !leading-10">
-        {t("landing:softwareDevelopment.title")}
+          {t("landing:softwareDevelopment.title")}
         </Typography>
         <BreadcrumbsComponent text="Software Development" />
-      </motion.div>
+      </AnimationFrame>
     </Box>
   );
 };
